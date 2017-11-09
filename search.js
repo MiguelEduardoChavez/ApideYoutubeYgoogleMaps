958f52568b41e6e72886f929a2ed485e1fdd0cfe
MiguelEduardoChavez <lalo_super@live.com>
2017-11-09 01:35:58 -0600

    estilo controlado por bootstrap 4, modificacion en el input de la cantidad de videos

--- search.js
+++ search.js
@@ -1,37 +1,41 @@
-// Your use of the YouTube API must comply with the Terms of Service:
-// https://developers.google.com/youtube/terms
-// Called automatically when JavaScript client library is loaded.
 var html = "";
 var clave = 'AIzaSyAvMwJrYgnwyepvkDhCHBXOgi49clWWM7M';
-//var texto = contenido.replace(/<[^>]*>?/g, '');;
 
 function onClientLoad() {
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
-// Called automatically when YouTube API interface is loaded (see line 9).
 function onYouTubeApiLoad() {
     gapi.client.setApiKey('AIzaSyAvMwJrYgnwyepvkDhCHBXOgi49clWWM7M');
 }
+function validar(){
+    html ="";
+    document.getElementById('errores').innerHTML = html;
+    var numResultados = document.getElementById('resultados').value;
+    numResultados = Number(numResultados);
+    console.log(numResultados);
+    if(numResultados <0 || numResultados>50){
+        
+        html += '<div class="alert alert-danger">Busqueda por default, para la proxima introduce un numero mayor a 0 y menor a 50</div>';
+        document.getElementById('errores').innerHTML = html;
+        numResultados = 5;
+    }
+    return numResultados;
+}
 
-// Called when the search button is clicked in the html code
 function search() {
     var query = document.getElementById('query').value;
-    //var contenido = document.getElementById('info');
-    console.log(query);
-    var numResultados = document.getElementById('resultados').value;
-   // console.log(Number(numResultados));
+    var numR = validar();
         var request = gapi.client.youtube.search.list({
             part: 'snippet',
             type: 'video',
             q: query,
-            maxResults: numResultados,
+            maxResults: numR,
             order: 'videoCount'
         });
 
         request.execute(onSearchResponse);
  
 }
-// Triggered by this line: request.execute(onSearchResponse);
 
 
 function onSearchResponse(response) {
@@ -62,34 +66,38 @@ function ubicacion(itemd) {
             var id_video = itemd.id.videoId;
             //var canal = itemd.snippet.channelTitle;
             var fecha = itemd.snippet.publishedAt;
-            
-            html += '<div><iframe src=\"//www.youtube.com/embed/' + id_video + '\" allowfullscreen width="600" height="400"></iframe>';
+            html += '<div class="row"><div class="col-md-6">';
+            //contenido del video
+            html += '<iframe src=\"//www.youtube.com/embed/' + id_video + '\" allowfullscreen width="600" height="400"></iframe>';
             html += '<h3>Titulo del Video: </h3>' + titulo_video;
             html += '<h4>Fecha de Subida: </h4>' + moment(fecha).format('DD/MM/YYYY');
-            html += '<h4>Localización</h4>';
+            html += '<hr class="hr-danger"/><hr></div>';//fin del contenedir del video
+
+            //contenido del mapa
+            html += '<div class="col-md-6">';
+          //  html += '<h4>Localización</h4>';
             if(item.recordingDetails) {
                 try {
                     var latitud = item.recordingDetails.location.latitude;
                     var longitud = item.recordingDetails.location.longitude;
                     if(latitud == undefined || longitud == undefined){
-                        html +='<h5>Esta busqueda no tiene latitud y longitud</h5>';
+                        html += '<div class="alert alert-danger"><h5>Esta busqueda no tiene latitud y longitud</h5></div>';
                     }else{
+                        
+                        var ruta = 'https://maps.googleapis.com/maps/api/staticmap?zoom=20&size=600x400&maptype=roadmap&markers=color:green%7Clabel:L%7C'+latitud+','+longitud+'&key='+clave;
+                        html +='<img src='+ruta+'>' ;
                         html += '<h5>Latitud: </h5>' + latitud;
-                        html += '<h5>Longitud: </h5>' + longitud + '</div>'; 
-                        var ruta = 'https://maps.googleapis.com/maps/api/staticmap?zoom=20&size=700x600&maptype=roadmap&markers=color:green%7Clabel:L%7C'+latitud+','+longitud+'&key='+clave;
-                        html +=  '<div class="mapa">'
-                        +'<img src='+ruta+'>'
-                        +'</div>';
+                        html += '<h5>Longitud: </h5>' + longitud ; 
 
                     }
                 } catch(err) {
                     
                 }
             }else
-                html +='<h5>Esta busqueda no tiene latitud y longitud</h5>';
-                
+                html +='<div class="alert alert-danger"><h5>Esta busqueda no tiene latitud y longitud</h5></div>';
+            html +='</div></div>';//fin de bloque de una busqued
             
-            html +='<hr>';
+            
         }
         document.getElementById('info').innerHTML = html;
     });
